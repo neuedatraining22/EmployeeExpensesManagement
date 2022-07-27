58ef37f2eac62437a711432b9e76d0a48004fa49
Matt <matt@multicode.co.uk>
2022-07-27 16:32:14 +0000

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Employee employee1 = new Employee(1,"Mr","Matt","Thornfield","Trainer","Tech Education");
+        Employee employee2 = new Employee(2, "Mrs", "Jenny", "Smith", "Manager", "Tech Education");
         System.out.println(employee1.getFirstName());
         System.out.println(employee1.getJobTitle());
     }
