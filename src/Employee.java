908819c7cea5b1ec49ab7aa86d95e8b2930dabf5
Matt <matt@multicode.co.uk>
2022-07-27 15:42:39 +0000

    employee

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+
+    private int id;
+    private String title;
+    private String firstName;
+    private String surname;
+    private String jobTitle;
+    private String department;
+
+
+}
