7a5fc582cb12edb92018323e2100ed7594d890e8
Matt <matt@multicode.co.uk>
2022-07-29 13:28:43 +0000

    ex1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,16 @@ public class Main {
         Employee employee2 = new Employee(2, "Mrs", "Jenny", "Smith", "Manager", "Tech Education");
         System.out.println(employee1.getFirstName());
         System.out.println(employee1.getJobTitle());
+
+        ExpenseClaim expenseClaim = new ExpenseClaim(24,642,"2021-09-01",26.99);
+        System.out.println(expenseClaim.getEmployeeId());
+        expenseClaim.setPaid(true);
+        System.out.println(expenseClaim.isPaid());
+        expenseClaim.setApproved(true);
+        expenseClaim.setPaid(true);
+        System.out.println(expenseClaim.isPaid());
+
+        ExpenseItem expenseItem = new ExpenseItem(24, 102, "hotel", "The Grand Hotel", 69.99);
+        System.out.println(expenseItem.getDescription());
     }
 }
