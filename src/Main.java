eee3021f53cced8f4b9ae1c6d9c9088899073474
Matt <matt@multicode.co.uk>
2022-08-10 14:13:35 +0000

    lists

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,60 @@ import domain.ExpenseClaim;
 import domain.ExpenseItem;
 import domain.ExpenseType;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
         Employee employee1 = new Employee(1,"Mr","Matt","Thornfield","Trainer","Tech Education");
         Employee employee2 = new Employee(2, "Mrs", "Jenny", "Smith", "Manager", "Tech Education");
-        System.out.println(employee1.getFirstName());
-        System.out.println(employee1.getJobTitle());
+        Employee employee3 = new Employee(3,"Mr","Matt","Jones","Trainer","Tech Education");
+        Employee employee4 = new Employee(4, "Mrs", "Jenny", "Green", "Manager", "Tech Education");
 
-        ExpenseClaim expenseClaim = new ExpenseClaim(24,642,new Date(),26.99);
-        System.out.println(expenseClaim.getEmployeeId());
-        expenseClaim.setPaid(true);
-        System.out.println(expenseClaim.isPaid());
-        expenseClaim.setApproved(true);
-        expenseClaim.setPaid(true);
-        System.out.println(expenseClaim.isPaid());
+        ArrayList<Employee> employeesList = new ArrayList<>();
+        //HashSet<Employee> employeesList = new HashSet<>();
+        employeesList.add(employee1);
+        System.out.println(employeesList.size());
+        employeesList.add(employee2);
+        System.out.println(employeesList.size());
+        employeesList.add(employee3);
+        employeesList.add(employee4);
 
-        ExpenseItem expenseItem = new ExpenseItem(24, 102, ExpenseType.ACCOMODATION, "The Grand Hotel", 69.99);
-        System.out.println(expenseItem.getDescription());
-        System.out.println(new Date());
+        Employee removedEmployee = employeesList.remove(0);
+        System.out.println(removedEmployee);
+        System.out.println(employeesList.size());
+        System.out.println(employeesList.get(0));
+
+        for(int i = 0; i < employeesList.size(); i++) {
+            System.out.println(employeesList.get(i));
+        }
+
+        employeesList.add(employee3);
+        employeesList.add(employee3);
+        employeesList.add(employee3);
+
+        for(Employee e : employeesList) {
+            System.out.println(e);
+        }
+
+        System.out.println(employeesList.size());
+
+//        System.out.println(employee1.getFirstName());
+//        System.out.println(employee1.getJobTitle());
+//
+//        ExpenseClaim expenseClaim = new ExpenseClaim(24,642,new Date(),26.99);
+//        System.out.println(expenseClaim.getEmployeeId());
+//        expenseClaim.setPaid(true);
+//        System.out.println(expenseClaim.isPaid());
+//        expenseClaim.setApproved(true);
+//        expenseClaim.setPaid(true);
+//        System.out.println(expenseClaim.isPaid());
+//
+//        ExpenseItem expenseItem = new ExpenseItem(24, 102, ExpenseType.ACCOMODATION, "The Grand Hotel", 69.99);
+//        System.out.println(expenseItem.getDescription());
+//        System.out.println(new Date());
 
 
     }
